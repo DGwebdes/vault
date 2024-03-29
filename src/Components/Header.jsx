21a2af46d8c98d28114c06f914e26b2c970729f9
DGwebdes <alan.bilik8@gmail.com>
2024-03-29 22:41:52 +0000

    edited logo url

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <nav>
-            <img src='../public/cubes.png' alt="logo"/>
+            <img src='https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="logo"/>
             <ul>
                 <li><Link to={'/'}>Root</Link></li>
                 {/* <li><Link to={'/portfolio'}>Portfolio</Link></li> */}
