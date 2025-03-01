3b0fef74c84339d5bc5408a5abfdf94bee1c4f0b
DGwebdes <alan.bilik8@gmail.com>
2025-03-01 06:06:02 +0000

    updates to the css remove styles

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,13 +1,12 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'
-import Spinner from '../Components/Spinner';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Spinner from "../Components/Spinner";
 
 const Homepage = () => {
     const [loading, setLoading] = useState(true);
     const [posts, setPosts] = useState([]);
-    const BASE_URL = 'https://my-blog-server-bn2n.onrender.com';
+    const BASE_URL = "https://my-blog-server-bn2n.onrender.com";
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -15,43 +14,55 @@ const Homepage = () => {
                 const response = await axios.get(`${BASE_URL}/api/posts`);
                 setPosts(response.data);
             } catch (err) {
-                setLoading(true)
-                console.log(`Something went Wrong ${err}`)
+                setLoading(true);
+                console.log(`Something went Wrong ${err}`);
             } finally {
-                setLoading(false)
+                setLoading(false);
             }
         };
 
         fetchPosts();
-    }, [])
+    }, []);
 
-    return (
-     loading ? (<Spinner />) : (
+    return loading ? (
+        <Spinner />
+    ) : (
         <main>
             <section className="hero">
                 <h1>Dielan&apos;s Vault.</h1>
                 <p>
-                    Documenting my adventures as a [insert label], [adjective] and [relatable adjective] person.
+                    Documenting my adventures as a [insert label], [adjective]
+                    and [relatable adjective] person.
                 </p>
             </section>
             <section className="content">
-            <ul>
-                {
-                    posts.map((post) => (
+                <ul>
+                    {posts.map((post) => (
                         <li key={post.sys.id}>
                             <Link to={`/posts/${post.sys.id}`}>
                                 <div className="spin"></div>
-                                <img className='post-img' src={post.fields.postImage.fields.file.url} alt="post-banner" />
-                                <h2 className='text-shadow'>{post.fields.title}</h2>
+                                <img
+                                    className="post-img"
+                                    src={post.fields.postImage.fields.file.url}
+                                    alt="post-banner"
+                                />
+                                <h2 className="text-shadow">
+                                    {post.fields.title}
+                                </h2>
                             </Link>
-                            <p><small>Date: {new Date(post.fields.publishDate).toLocaleDateString()}</small></p>
+                            <p>
+                                <small>
+                                    Date:{" "}
+                                    {new Date(
+                                        post.fields.publishDate
+                                    ).toLocaleDateString()}
+                                </small>
+                            </p>
                         </li>
-                    ))
-                }
-            </ul>
+                    ))}
+                </ul>
             </section>
         </main>
-    )
     );
 };
 
