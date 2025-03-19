8d39d28a2afe46274ea91758eb7bd7bafa8e4019
DGwebdes <alan.bilik8@gmail.com>
2025-03-19 16:28:03 +0000

    changed server url to Railway

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -6,7 +6,7 @@ import Spinner from "../Components/Spinner";
 const Homepage = () => {
     const [loading, setLoading] = useState(true);
     const [posts, setPosts] = useState([]);
-    const BASE_URL = "https://my-blog-server-bn2n.onrender.com";
+    const BASE_URL = "my-blog-server-production-35c7.up.railway.app";
 
     useEffect(() => {
         const fetchPosts = async () => {
